dfeecde189406d65fd204f94644a47f135966371
Oleksandr Vykhor <sashavihor67@gmail.com>
2024-12-28 22:42:36 +0100

    added theme and GlobalStyles

---
+++ src/Assets/Styles/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    black: "#18181B",
+    grey: "#E4E6F0",
+    lightGrey: "#F5F5FA",
+    darkerGrey: "#7E839A",
+    white: "#FFFFFF",
+    blue: "#0044CC",
+    lightBlue: "#D6E4FF",
+  },
+  breakpoint: {
+    desktopMax: "1368px",
+    desktop: "1200px",
+    ipod: "1024px",
+    large: "767px",
+    mini: "600px",
+    micro: "400px",
+  },
+};
