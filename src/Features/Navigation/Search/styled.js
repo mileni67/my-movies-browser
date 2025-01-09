c3a5f1817f43ca32a627f5f364264d0eb943b7bf
Oleksandr Vykhor <sashavihor67@gmail.com>
2025-01-09 20:16:02 +0100

    add media

--- src/Features/Navigation/Search/styled.js
+++ src/Features/Navigation/Search/styled.js
@@ -2,12 +2,17 @@ import styled from "styled-components";
 import inputSearchIcon from "../../../Assets/Images/inputSearchIcon.svg";
 
 export const Wrapper = styled.div`
-  margin-left: auto;
-  margin-right: 20px; 
+  margin-left: 387px;
+  flex-shrink: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.desktop}) {
+    margin-left: 20px;
+  }
 `;
 
 export const Input = styled.input`
-  width: 432px;
+  width: 100%;
+  max-width: 432px;
   height: 48px;
   border-radius: 33px;
   border: 1px solid ${({ theme }) => theme.colors.gray};
@@ -17,4 +22,8 @@ export const Input = styled.input`
   background-repeat: no-repeat;
   background-position: 24px center;
   background-size: 24px 24px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.desktop}) {
+    width: 300px,
+  }
 `;
